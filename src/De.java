1042343c2ea7ae1da4a2b7282af56ba7adc296d3
Bebel9445 <bebel9445@gmail.com>
2025-05-07 11:36:57 +0200

    Ajout de la classe De pour gerer les lancer de n de de m faces

---
+++ src/De.java
@@ -0,0 +1,41 @@
+
+import java.util.Random;
+
+public class De {
+
+    private final String _name;
+    private final int _number;
+    private final int _faces;
+
+    public De() {
+        _name = "1d6";
+        _number = 1;
+        _faces = 6;
+    }
+
+    public De(int faces) {
+        _name = "1d" + faces;
+        _number = 1;
+        _faces = faces;
+    }
+
+    public De(int number, int faces) {
+        _name = number+"d"+faces;
+        _number = number;
+        _faces = faces;
+    }
+
+    public int roll() {
+        Random rand = new Random();
+        int total = 0;
+        for (int i = 0; i < this._number; i++) {
+            total += rand.nextInt(this._faces);
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return _name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Bienvenue dans DOOnjon et Dragons");
+        De de = new De(2, 20);
+        System.out.println(de);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Lancé " + (i+1) + " de " + de + " : " + de.roll());
+        }
     }
 }
