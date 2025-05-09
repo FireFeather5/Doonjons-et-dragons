ecc00ff5961483737da0b3704bd2413ed052651d
Capucine Dausse <capucine.dausse@gmail.com>
2025-05-09 13:18:55 +0200

    Début classe monstre

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,5 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Bienvenue dans DOOnjon et Dragons");
 
-        Personnage test = new Personnage("Pap", new Humain(), new Clerc());
-
     }
 }
---
+++ src/monstres/Monstre.java
@@ -0,0 +1,19 @@
+package monstres;
+
+import de.De;
+
+public class Monstre {
+    private String _espece;
+    private int _numero;
+    private De _attaque;
+    private int[] _stats = {0, 0, 0, 0, 0, 0};
+    //                  pv, for, dex, vit, ini, arm
+    //a modifier
+    //private armure _classe;
+
+    public Monstre(String espece, int portAtt, De degAtt, De characterist)
+    {
+        
+    }
+
+}
